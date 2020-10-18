dd4341c8e58123eb350f50c75edf58c7fdb58b73
ZaneStaufer <stauffer_zane@columbusstate.edu>
2020-10-18 16:11:21 -0400

    implement pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# stauffer_midterm
+# Comic Website
+This website allows you to view the [xkcd](https://xkcd.com/) comic of the day. It also allows you to view a random comic.
--- index.js
+++ index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var fetch = require('node-fetch');
 
 var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
 const port = process.env.port || 3000;
 
-//
+app.get('/', (req, res) => {
+    fetch('https://xkcd.com/info.0.json')
+        .then(res => res.json())
+        .then(data => {
+            res.render('index', {data:data});
+        });
+});
+
+app.get('/random', (req, res) => {
+    fetch('https://xkcd.com/info.0.json')
+        .then(res => res.json())
+        .then(data => {
+            var max = data.num + 1;
+            var min = 1;
+            var rand = Math.floor(Math.random() * (max - min) + min);
+            fetch(`https://xkcd.com/${rand}/info.0.json`)
+                .then(randRes => randRes.json())
+                .then(data => {
+                    res.render('random', {data:data});
+                });
+        });
+});
 
 app.listen(port, function(){});
